e3e527c78b6446482376de4978eb8291e309cf22
DamianBasista <damian.basista@gmail.com>
2018-05-08 09:17:55 +0200

    add new function

--- javascript.js
+++ javascript.js
@@ -12,8 +12,8 @@ const ballSize = 20;
 let ballX=cw/2 - ballSize/2;
 let ballY=ch/2 - ballSize/2;
 
-const paddelHeight = 100;
-const paddelWidth = 20;
+const paddleHeight = 100;
+const paddleWidth = 20;
 
 const playerX = 70;
 const aiX = 910;
@@ -29,12 +29,12 @@ let ballSpeedY = 4;
 
 function player(){
     ctx.fillStyle="#7fff00";
-    ctx.fillRect(playerX, playerY, paddelWidth, paddelHeight);    
+    ctx.fillRect(playerX, playerY, paddleWidth, paddleHeight);    
 }
 
 function ai(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(aiX, aiY, paddelWidth, paddelHeight);    
+    ctx.fillRect(aiX, aiY, paddleWidth, paddleHeight);    
 }
 
 function ball(){
@@ -68,31 +68,55 @@ function table(){
 topCanvas = canv.offsetTop;
 
 function playerPosition(e){
-    playerY = e.clientY - topCanvas - paddelHeight/2;
+    playerY = e.clientY - topCanvas - paddleHeight/2;
 
-    if(playerY >= ch - paddelHeight){
-        playerY = ch - paddelHeight
+    if(playerY >= ch - paddleHeight){
+        playerY = ch - paddleHeight
     }
 
     if(playerY <= 0){
         playerY = 0;
     }
 
-    aiY = playerY;
+    // aiY = playerY;
 }
 
 function speedUp(){
 
     if(ballSpeedX > 0 && ballSpeedX < 16){
-        ballSpeedX += 1;
+        ballSpeedX += .2;
     }else if(ballSpeedX < 0 && ballSpeedX > -16){
-        ballSpeedX -= 1;
+        ballSpeedX -= .2;
     }
 
     if(ballSpeedY > 0 && ballSpeedY < 16){
-        ballSpeedY += 1;
+        ballSpeedY += .2;
     }else if(ballSpeedY < 0 && ballSpeedY > -16){
-        ballSpeedY -= 1;
+        ballSpeedY -= .2;
+    }
+}
+ 
+function aiPosition(){ 
+    const middlePaddle = aiY + paddleHeight/2;
+    const middleBall = ballY + ballSize/2;
+    if(ballX > 500){
+        if(middlePaddle - middleBall > 200){
+            aiY -= 15;
+        }else if(middlePaddle - middleBall > 50){
+            aiY -= 5;
+        }
+        
+        if(middlePaddle - middleBall < -200){
+            aiY += 15;
+        }else if(middlePaddle - middleBall < -50){
+            aiY += 5;
+        }
+    }else if(ballX <= 500 && ballX > 150){
+        if(middlePaddle - middleBall > 100){
+            aiY -= 3;
+        }else if(middlePaddle - middleBall < -100){
+            aiY += 3;
+        }
     }
 }
 
@@ -103,5 +127,6 @@ function game(){
     ball();
     player();
     ai();
+    aiPosition();
 }
 setInterval(game, 1000/ 60);
